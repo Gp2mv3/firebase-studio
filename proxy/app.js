f1c312ea134a52df925fa99b4d0cc08c399dc780
Gp2mv3 <nicolascognaux@gmail.com>
2020-10-11 22:28:30 +0200

    🔥 Removed useless routes

--- proxy/app.js
+++ proxy/app.js
@@ -7,7 +7,6 @@ var cors = require('cors');
 
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -20,7 +19,6 @@ app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
