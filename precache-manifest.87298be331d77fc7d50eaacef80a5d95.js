ae0a99f4d0998b50e5882241f286f3ff477a4ac1
Gp2mv3 <nicolascognaux@gmail.com>
2020-11-03 00:07:31 +0100

    Updates

---
+++ precache-manifest.87298be331d77fc7d50eaacef80a5d95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f60a36ad638dc92e4d06b4b22420e3",
+    "url": "/firebase-studio/index.html"
+  },
+  {
+    "revision": "1f3c692898b900bee935",
+    "url": "/firebase-studio/static/css/main.d1dc5b4d.chunk.css"
+  },
+  {
+    "revision": "9f5966528e8ba107e353",
+    "url": "/firebase-studio/static/js/2.8e2a10e0.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/firebase-studio/static/js/2.8e2a10e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f3c692898b900bee935",
+    "url": "/firebase-studio/static/js/main.39a5609c.chunk.js"
+  },
+  {
+    "revision": "5e20301cd2bca9c17a6d",
+    "url": "/firebase-studio/static/js/runtime-main.6a0bcbb0.js"
+  }
+]);
